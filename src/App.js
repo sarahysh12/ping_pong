f1f5219286b628d0e7f7aa23d044e23d7eb9da38
sarahysh12 <saraysh22@gmail.com>
2020-06-28 23:06:22 -0700

    finish the game version 1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import BoardGame from './containers/Boardgame/Boardgame';
 import Table from './containers/Table/Table';
 import './App.css';
 
@@ -6,6 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        {/* <BoardGame/> */}
         <Table/>
       </div>
     );
--- src/containers/Table/Table.js
+++ src/containers/Table/Table.js
@@ -10,52 +10,58 @@ class Table extends Component {
     state = {
         score: 0,
         isMobile: false,
-        timer: 0,
-        isGameOver: false,
-        start: null
+        isServe: true,
+        hitCount: 0,
+        highScore: 0,
+        lastHitTime: Date,
+        isGameStarted: false
     }
+
     permissionGranted = false;
-    
-    playerPaddle = null;
+    paddleWidth = 100
+    paddleX = null;
     playerPaddleVel = null;
-    
     ballVelocity = null;
     ball = null;
-
-    componentDidMount = () => {   
-        setInterval(this.timer, 1000);
-    }
-
-    timer = () => {
-        this.setState({ timer: this.state.timer+1 });
-    }
-
-    resetScore = () => {
-        this.setState({score: 0}); 
-    }
+    outRef = React.createRef();
+    inRef = React.createRef();
+    overRef = React.createRef();
 
     setup = (p5) => {
-        p5.createCanvas(p5.windowWidth, p5.windowHeight);
-        this.playerPaddle = p5.width / 2 - 50;
+        this.resetGame(p5, null)();
+        this.grantPermission(p5);
+    };
 
-        this.ball = p5.createVector(p5.width/2, p5.height/2);
-        this.ballVelocity = p5.createVector(Math.random(-1,1), Math.random(-1,1));
-        this.ballVelocity.mult(3);
-
-      if(typeof(DeviceOrientationEvent) !== 'undefined' && typeof(DeviceOrientationEvent.requestPermission) === 'function') {
-          this.setState({isMobile: true});
-        // ios 13 device
-        DeviceOrientationEvent.requestPermission()
+    grantPermission = (p5) => {
+        if(typeof(DeviceOrientationEvent) !== 'undefined' && typeof(DeviceOrientationEvent.requestPermission) === 'function') {
+            this.setState({isMobile: true});
+            // ios 13 device
+            DeviceOrientationEvent.requestPermission()
             .catch((error) => {
                 //show permission dialog only the first time
                 let button = p5.createButton('Grant Access');
                 button.style('font-size', '24px');
                 button.center();
-                button.mousePressed(this.requestAccess);
+                button.mousePressed(this.requestAccess(button));
                 throw error;
             })
             .then(() => {
                 // on any subsequent visits
+                p5.textSize(50);
+                p5.text('Score: 0' , p5.width/2, (p5.height/2)-50);
+                let button = p5.createButton('Let\'s Play');
+                button.style('font-size', '24px');
+                button.position(p5.width/2-75, (p5.height/2)+50);
+                button.mousePressed(() => {
+                    this.setState({isGameStarted: true});
+                    button.remove();
+                    this.outRef.current.play();
+                    this.outRef.current.pause();
+                    this.inRef.current.play();
+                    this.inRef.current.pause();
+                    this.overRef.current.play();
+                    this.overRef.current.pause();
+                });
                 this.permissionGranted =  true;
             })
         } else {
@@ -63,13 +69,56 @@ class Table extends Component {
             p5.textSize(48);
             p5.text('non ios 13 device', 100,100);
         }
-    };
+    }
 
+    requestAccess = (button) => {
+        return () => {
+            DeviceOrientationEvent.requestPermission()
+                .then(response => {
+                    if(response === 'granted') {
+                        this.permissionGranted = true; 
+                    } else{
+                        this.permissionGranted =  false;
+                    }
+                })
+                .catch(console.error);
+                button.remove();
+        }
+    }
 
-    resetGame = () => {
-        this.setState({isGameOver: false});
-        this.setState({score: 0});
-        this.setState({timer: 0});
+    gameOver = p5 => {
+        this.overRef.current.play();
+
+        if (this.state.score > this.state.highScore) {
+            this.setState({highScore: this.state.score})
+        }
+
+        p5.textSize(45);
+        p5.text('Game Over', p5.width/2-100, p5.height/2-200);
+        p5.text(`Score: ${this.state.score}` , p5.width/2-100, (p5.height/2)-50);
+        p5.text(`HighScore: ${this.state.highScore}` , p5.width/2-100, (p5.height/2)+100);
+        let button = p5.createButton('Play Again');
+        button.position(p5.width/2-50, (p5.height/2)+250);
+        button.size(100,40);
+        button.mousePressed(this.resetGame(p5, button));
+
+        p5.noLoop();
+    }
+
+    resetGame = (p5, button) => { 
+        return () => {
+            p5.createCanvas(p5.windowWidth, p5.windowHeight);
+            this.paddleX = p5.width / 2 - this.paddleWidth / 2;
+            this.ball = p5.createVector(p5.width/2, p5.height - 35);
+            this.ballVelocity = p5.createVector(0,0);
+            this.setState({isServe: true});
+            this.setState({score: 0});
+            this.setState({hitCount: 0}); 
+            if (button != null) {
+                button.remove()
+            }
+            p5.loop();
+        }
     }
 
     draw = p5 => {
@@ -79,22 +128,7 @@ class Table extends Component {
         let playerColor = p5.color('#3718b5');
         p5.fill(playerColor);
         p5.noStroke();
-        p5.rect(this.playerPaddle, p5.height - 20, 100, 10, 20);
-
-        if(this.state.isMobile) {
-            this.playerPaddleVel = p5.constrain(p5.rotationY, -3, 3);
-            this.playerPaddle += this.playerPaddleVel*5;
-            this.playerPaddle = p5.constrain(this.playerPaddle, 0, p5.width-100);
-        } else {
-            if(p5.keyIsDown(p5.LEFT_ARROW)) {
-                this.playerPaddleVel -= 5;
-            }
-            if(p5.keyIsDown(p5.RIGHT_ARROW)) {
-                this.playerPaddleVel += 5;
-            }
-            this.playerPaddleVel *= 0.4;
-            this.playerPaddle += this.playerPaddleVel
-        }
+        p5.rect(this.paddleX, p5.height - 20, 100, 10, 20);
 
         /* Draw mesh */
         p5.stroke(255);
@@ -105,62 +139,90 @@ class Table extends Component {
         let ballColor = p5.color(255);
         p5.fill(ballColor);
         p5.ellipse(this.ball.x, this.ball.y, 30);
-        // p5.text(this.state.score, p5.width/2, 50);
-        // p5.text(this.state.timer, p5.width/2, 100);
+        p5.textSize(20);
+        // p5.text(this.state.score, p5.width/2, 100);
 
-        /* Move ball */
+        /* Collosion with paddle */
+        if(p5.height - this.ball.y <= 20 && p5.height - this.ball.y >= 0) { 
+            if( this.ball.x >= this.paddleX && this.ball.x <= this.paddleX+this.paddleWidth) {
+                this.ballVelocity.y *= -1;
+                this.inRef.current.play();
+                this.setState({lastHitTime: new Date()});
+                const newHit = this.state.hitCount;
+                this.setState({hitCount: this.state.hitCount+1});
+            } else {
+                this.gameOver(p5);
+            }
+        } 
+
+        /* Move Paddle  */
+        if(this.state.isMobile) {
+            if(!this.state.isServe) {
+                this.playerPaddleVel = p5.constrain(p5.rotationY, -3, 3);
+                this.paddleX += this.playerPaddleVel*2;
+                this.paddleX = p5.constrain(this.paddleX, 0, p5.width-this.paddleWidth);
+            }
+        } else {
+            if(this.state.isServe && p5.keyIsDown(p5.UP_ARROW)) {
+                this.ballVelocity.x = -2;
+                this.ballVelocity.y = -2;
+                this.setState({isServe: false});
+                this.setState({lastHitTime: new Date()})
+            }
+            if(!this.state.isServe) {
+                if(p5.keyIsDown(p5.LEFT_ARROW)) {
+                    this.playerPaddleVel -= 5;
+                }
+                if(p5.keyIsDown(p5.RIGHT_ARROW)) {
+                    this.playerPaddleVel += 5;
+                }
+                this.playerPaddleVel *= 0.4;
+                this.paddleX += this.playerPaddleVel;
+            }
+        }
+
         /* Collision with left and right */
         if (this.ball.x > p5.width || this.ball.x < 0) {
             this.ballVelocity.x *= -1;
         }
         /* Robot play */
         if (this.ball.y <= 20) {
-            this.ballVelocity.y *= -1;
-            //TODO change this timer for desktop
-            if(this.state.timer >= 1000) {
-                this.setState({isGameOver: true});
-                let audio = new Audio(gameOverSound);
-                // audio.play();
-                this.resetGame();
-                return;
+            this.outRef.current.play();
+            let coef = 1;
+            if(this.state.hitCount ==  10) {
+                coef = 1.1;
+                this.setState({hitCount: 0})
             }
-            let audio = new Audio(ballIncomingSound);
-            // audio.play();
-        }
-        /* Collosion with paddle */
-        
-        if(p5.height - this.ball.y <= 30 && p5.height - this.ball.y >= 0) { 
-            // console.log(p5.height - this.ball.y, this.ball.x, this.playerPaddle);
-            if( this.ball.x > this.playerPaddle && p5.height - this.ball.x < this.playerPaddle+100) {
-                this.ballVelocity.y *= -1;
-                let audio = new Audio(ballOutgoingSound);
-                // audio.play();
+            this.ballVelocity.y *= -1 * coef ;
+            if (new Date() - this.state.lastHitTime > 3000) {
+               this.gameOver(p5);
+            } else {
                 const newScore = this.state.score;
                 this.setState({score: newScore+1});
-                
             }
-        } 
+        }
+        
+        /* First serve */
+        if(this.state.isServe && p5.pAccelerationZ - p5.accelerationZ > 2) {
+            this.ballVelocity.x = Math.tanh(p5.rotationX - p5.pRotationX);
+            this.ballVelocity.y = -3;
+            this.setState({isServe: false});
+            this.setState({lastHitTime: new Date()});
+        }
+        
+        /* Move Ball */
+        this.ball.x += this.ballVelocity.x*3;
+        this.ball.y += this.ballVelocity.y*3;
 
-        this.ball.x += this.ballVelocity.x;
-        this.ball.y += this.ballVelocity.y;
     };
 
-    requestAccess = () => {
-        DeviceOrientationEvent.requestPermission()
-            .then(response => {
-                if(response === 'granted') {
-                    this.permissionGranted = true; 
-                } else{
-                    this.permissionGranted =  false;
-                }
-            })
-            .catch(console.error);
-        // this.remove();
-    }
-   
     render() {
       return (
         <div className="Game">
+            <audio ref={this.inRef} src={ballIncomingSound} />
+            <audio ref={this.outRef} src={ballOutgoingSound} />
+            <audio ref={this.overRef} src={gameOverSound} />
+            {/* <p>{this.state.deviceRotationY} - {this.state.newY}</p> */}
             <Sketch setup={this.setup} draw={this.draw} />
         </div>
       );
